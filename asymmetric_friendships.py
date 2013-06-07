8ed0f84f4463cc707523a8d2b7172e6125609d0a
Timothy Gaweco <tim.gaweco@gmail.com>
2013-06-06 23:57:45 -0600

    Update asymmetric_friendships.py

--- asymmetric_friendships.py
+++ asymmetric_friendships.py
@@ -5,17 +5,21 @@ import sys
 
 mr = MapReduce.MapReduce()
 
+# The input are key-value pairs of two friends. The output are tuples of 
+# asymmetric friendships (that is, one person follows a person but isn't followed back by that same person).
+
 def mapper(record):
-    #key: person_a
-    #value: person_b
+    #key: person_a, person_b
+    #value: 1
     person_a = record[0]
     person_b = record[1]
+
     mr.emit_intermediate((person_a, person_b), 1)
     mr.emit_intermediate((person_b, person_a), 1)
 
 def reducer(key, list_of_friends):
     #key: person
-    #value: friend
+    #value: n/a
     if len(list_of_friends) < 2:
         mr.emit((key))
 
