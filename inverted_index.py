a5b0ca01fc4efac294b10c8fb72184bc0c7d1c3c
Timothy Gaweco <tim.gaweco@gmail.com>
2013-06-06 23:59:22 -0600

    Update inverted_index.py

--- inverted_index.py
+++ inverted_index.py
@@ -1,9 +1,12 @@
-__author__ = 'tgameco'
+__author__ = 'tgaweco'
 
 import MapReduce
 import sys
 
 mr = MapReduce.MapReduce()
+
+# The input are key-value pairs of document id's and accompanying text. 
+# The output are key-value pairs of words and the documents they're found in
 
 def mapper(record):
     #key: word
@@ -11,6 +14,7 @@ def mapper(record):
     document_id = record[0]
     txt = record[1]
     t = txt.split()
+    
     for word in t:
         mr.emit_intermediate(word, document_id)
 
@@ -18,6 +22,7 @@ def reducer(key, list_of_document_ids):
     #key: words
     #value: list of document ids associated with the word
     list_of_ids = list()
+    
     for d in list_of_document_ids:
         list_of_ids.append(d)
     mr.emit((key,list(set(list_of_ids))))
